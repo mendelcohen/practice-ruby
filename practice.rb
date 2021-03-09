9726e408a32ffd68d87ac242a07c5d70f50b377a
Mendel Cohen <mmhakohen@gmail.com>
2021-03-08 21:01:51 -0500

    solves array of string combinations

--- practice.rb
+++ practice.rb
@@ -740,9 +740,25 @@ def combine_strings(array1, array2)
 end
 # p combine_strings(["a", "b", "c"], ["d", "e", "f", "g"])
 
-Given ONE array of strings, return a new array that contains every combination of each string with every other string in the array.
 
-Input: ["a", "b", "c", "d"]
-Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
+# Given ONE array of strings, return a new array that contains every combination of each string with every other string in the array.
 
+# Input: ["a", "b", "c", "d"]
+# Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
 
+def array_of_combinations(array)
+  new_array = []
+  i = 0
+  while i < array.length
+    j = 0
+    while j < array.length
+      if array[i] != array[j]
+        new_array << array[i] + array[j]
+      end
+      j += 1
+    end
+    i += 1
+  end
+  return new_array
+end
+p array_of_combinations(["a", "b", "c", "d"])
