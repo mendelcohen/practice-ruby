2a7d1c6c877c1126cef19cd2bef92165b9ad563a
Mendel Cohen <mmhakohen@gmail.com>
2021-03-11 17:13:56 -0500

    solves order the whole menu

--- practice.rb
+++ practice.rb
@@ -991,4 +991,30 @@ def string_count(array)
   end
   return counted_strings
 end
-p string_count(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"])
+# p string_count(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"])
+
+
+
+# Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+
+# Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
+# Output: 14
+
+# Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+def sum_total_values(hash)
+  sum_total = 0
+  index = 0
+  hash.each do |_k, v|
+    sum_total += v
+  end
+  # while index < hash.length
+  #   sum_total += hash[index]
+  #   index += 1
+  # end
+  return sum_total
+end
+# p sum_total_values({"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2})
+
+
