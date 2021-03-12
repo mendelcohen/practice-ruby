233ba4231e01ebe3f261d40dfa1eebd336165d09
Mendel Cohen <mmhakohen@gmail.com>
2021-03-12 11:20:14 -0500

    solves popular posts

--- practice.rb
+++ practice.rb
@@ -1052,6 +1052,43 @@ def dna_to_rna(string)
   end
   return rna
 end
-p dna_to_rna("ACGTGGTCTTAA")
+# p dna_to_rna("ACGTGGTCTTAA")
 
 
+
+# Given an array of hashes that represent a list of social media posts, return a new array that only contains the posts that have at least 1000 likes.
+
+# Input: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+# ]
+
+# Output: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# ]
+
+def thousand_likes_posts(array)
+  thousand_likes = array.reject!{|post| post[:likes] < 1000}
+  return thousand_likes
+end
+p thousand_likes_posts([
+  {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+  {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+  {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+  {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+  ])
+
+
+def thousand_likes_posts(array)
+  thousand_likes = array.delete_if{|post| post[:likes] < 1000}
+  return thousand_likes
+end
+p thousand_likes_posts([
+  {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+  {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+  {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+  {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+  ])
