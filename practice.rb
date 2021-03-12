2de41944d3d7a9869d1b1ec02da980675466ed21
Mendel Cohen <mmhakohen@gmail.com>
2021-03-12 10:52:01 -0500

    solves rna transcription

--- practice.rb
+++ practice.rb
@@ -1018,3 +1018,40 @@ end
 # p sum_total_values({"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2})
 
 
+
+# Given a DNA strand, return its RNA complement (per RNA transcription).
+
+# Both DNA and RNA strands are a sequence of nucleotides. Here we're representing the sequences with single-letter characters (e.g. a sample strand may look like: "AGCA".)
+
+# Given a string representing a DNA strand, provide its transcribed RNA strand, according to the following pattern:
+
+# G becomes C
+# C becomes G
+# T becomes A
+# A becomes U
+
+# So based on all this, here's a sample input/output:
+
+# Input: 'ACGTGGTCTTAA'
+# Output: 'UGCACCAGAAUU'
+          
+def dna_to_rna(string)
+  rna = ""
+  string.each_char do |letter|
+    if letter === "G"        
+      rna += "C"
+    elsif letter === "C"
+      rna += "G"
+    elsif letter === "T"     
+      rna += "A"
+    elsif letter === "A"
+      rna += "U"
+    else
+      rna += letter
+    end
+  end
+  return rna
+end
+p dna_to_rna("ACGTGGTCTTAA")
+
+
