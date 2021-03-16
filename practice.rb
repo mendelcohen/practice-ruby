22b3b5bbb7bdad5200cf57341c9dbe26562326ea
Mendel Cohen <mmhakohen@gmail.com>
2021-03-16 11:57:40 -0400

    solves etl2

--- practice.rb
+++ practice.rb
@@ -1224,4 +1224,104 @@ def switch_key_value(hash)
   end
   return flipped_hash
 end
-p switch_key_value({"a" => 1, "b" => 2, "c" => 3})
+# p switch_key_value({"a" => 1, "b" => 2, "c" => 3})
+
+
+# You are given a hash in format #A, and you are to return the same data as a hash using format #B, as specified below:
+
+# Input:
+
+# {
+# 1 => ["A", "E", "I", "O", "U"]
+# }
+
+# Output:
+
+# {
+# 'a' => 1,
+# 'e' => 1,
+# 'i' => 1,
+# 'o' => 1,
+# 'u' => 1
+# }
+# Input:
+
+# {
+# 1 => ["A", "E"],
+# 2 => ["D", "G"]
+# }
+
+# Output:
+
+# {
+# 'a' => 1,
+# 'd' => 2,
+# 'e' => 1,
+# 'g' => 2
+# }
+
+# Input:
+
+# {
+# 1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+# 2 => ["D", "G"],
+# 3 => ["B", "C", "M", "P"],
+# 4 => ["F", "H", "V", "W", "Y"],
+# 5 => ["K"],
+# 8 => ["J", "X"],
+# 10 => ["Q", "Z"]
+# }
+
+# Output:
+
+# {
+# 'a' => 1,
+# 'b' => 3,
+# 'c' => 3,
+# 'd' => 2,
+# 'e' => 1,
+# 'f' => 4,
+# 'g' => 2,
+# 'h' => 4,
+# 'i' => 1,
+# 'j' => 8,
+# 'k' => 5,
+# 'l' => 1,
+# 'm' => 3,
+# 'n' => 1,
+# 'o' => 1,
+# 'p' => 3,
+# 'q' => 10,
+# 'r' => 1,
+# 's' => 1,
+# 't' => 1,
+# 'u' => 1,
+# 'v' => 4,
+# 'w' => 4,
+# 'x' => 8,
+# 'y' => 4,
+# 'z' => 10
+# }
+
+def key_value_exchange(hash)
+  new_hash = {}
+  hash.each do |k, v|
+    index = 0
+    while index < v.length
+      new_hash[v[index]] = k
+      index += 1
+    end
+  end
+  return new_hash.sort.to_h
+end
+p key_value_exchange({1 => ["A", "E", "I", "O", "U"]})
+p key_value_exchange({1 => ["A", "E"], 2 => ["D", "G"]})
+p key_value_exchange({
+  1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+  2 => ["D", "G"],
+  3 => ["B", "C", "M", "P"],
+  4 => ["F", "H", "V", "W", "Y"],
+  5 => ["K"],
+  8 => ["J", "X"],
+  10 => ["Q", "Z"]
+  })
