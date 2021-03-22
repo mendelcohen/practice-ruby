30f7f6d4ccc86078869d58f687f194c6b6672c39
Mendel Cohen <mmhakohen@gmail.com>
2021-03-22 10:58:45 -0400

    solves etl #4

--- practice.rb
+++ practice.rb
@@ -1706,6 +1706,61 @@ def sum_ten(array)
   end
   return false
 end
-p sum_ten([2, 5, 3, 1, 0, 7, 11])
-p sum_ten([1, 2, 3, 4, 5])
+# p sum_ten([2, 5, 3, 1, 0, 7, 11])
+# p sum_ten([1, 2, 3, 4, 5])
 
+
+
+# This is very similar to ETL #3, but you must now accomplish the task in linear time.
+
+# Given an array of Youtube videos, for example:
+
+# [
+# {title: 'How to Make Wood', author_id: 4, views: 6},
+# {title: 'How to Seem Perfect', author_id: 4, views: 111},
+# {title: 'Review of the New "Unbreakable Mug"', author_id: 2, views: 202},
+# {title: 'Why Pigs Stink', author_id: 1, views: 12}
+# ]
+
+# and an array of authors, for example:
+
+# [
+# {id: 1, first_name: 'Jazz', last_name: 'Callahan'},
+# {id: 2, first_name: 'Ichabod', last_name: 'Loadbearer'},
+# {id: 3, first_name: 'Saron', last_name: 'Kim'},
+# {id: 4, first_name: 'Teena', last_name: 'Burgess'},
+# ]
+
+# Return a new array of videos in the following format, and only include videos that have at least 100 views:
+
+# [
+# {title: 'How to Seem Perfect', views: 111, author_name: 'Teena Burgess' }
+# {title: 'Review of the New "Unbreakable Mug"', views: 202, author_name: 'Ichabod Loadbearer' },
+# ]
+
+def hundred_views(array1, array2)
+  new_array = []
+  array1.each do |video|
+    if video[:views] >= 100
+      array2.each do |author|
+        if video[:author_id] === author[:id]
+          video.delete(:author_id)
+          video[:author_name] = author[:first_name] + " " + author[:last_name]
+          new_array << video
+        end
+      end
+    end
+  end
+  return new_array
+end
+p hundred_views([
+  {title: 'How to Make Wood', author_id: 4, views: 6},
+  {title: 'How to Seem Perfect', author_id: 4, views: 111},
+  {title: 'Review of the New "Unbreakable Mug"', author_id: 2, views: 202},
+  {title: 'Why Pigs Stink', author_id: 1, views: 12}
+  ], [
+    {id: 1, first_name: 'Jazz', last_name: 'Callahan'},
+    {id: 2, first_name: 'Ichabod', last_name: 'Loadbearer'},
+    {id: 3, first_name: 'Saron', last_name: 'Kim'},
+    {id: 4, first_name: 'Teena', last_name: 'Burgess'},
+    ])
