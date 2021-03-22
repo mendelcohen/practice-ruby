54f3d80dacddc91d847d1648d26b246a08035b3a
Mendel Cohen <mmhakohen@gmail.com>
2021-03-22 10:34:25 -0400

    solves two sum II

--- practice.rb
+++ practice.rb
@@ -1673,5 +1673,39 @@ def non_repeating_letter(string)
   end
   return string.index(letter)
 end
-p non_repeating_letter("leetcode")
-p non_repeating_letter("loveleetcode")
+# p non_repeating_letter("leetcode")
+# p non_repeating_letter("loveleetcode")
+# p non_repeating_letter("lleetcode")
+# p non_repeating_letter("clovveleetode")
+
+
+
+# This is the same exercise as Two Sum I, but you must now solve it in linear time.
+
+# Given an array of numbers, return a new array containing just two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
+
+# Input: [2, 5, 3, 1, 0, 7, 11]
+# Output: [3, 7]
+
+# Input: [1, 2, 3, 4, 5]
+# Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
+
+def sum_ten(array)
+  hash = {}
+  sub_array = []
+  array.each do |int|
+    hash[int] = "yeah"
+  end
+  array.each do |k, _v|
+    hash.delete(k)
+    if hash.include?(10 - k)
+      sub_array << k
+      sub_array << 10 - k
+      return sub_array
+    end
+  end
+  return false
+end
+p sum_ten([2, 5, 3, 1, 0, 7, 11])
+p sum_ten([1, 2, 3, 4, 5])
+
